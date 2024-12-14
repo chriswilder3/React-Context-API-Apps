54633499283799af02a4952d558d3d8e33c7a7ab
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-14 21:55:20 +0530

    dark mode working correctly

--- Theme-Switcher-App/src/App.jsx
+++ Theme-Switcher-App/src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 
 import './App.css'
 import { ThemeProvider } from './contexts/Theme'
+import Card from './components/Card'
+import ThemeBtn from './components/ThemeBtn'
 
 function App() {
   const [ themeMode, setThemeMode] = useState("light")
@@ -32,11 +34,11 @@ function App() {
 
             <div className="w-full">
                   <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                      <Card />    
+                    <ThemeBtn / >  
                   </div>
 
                   <div className="w-full max-w-sm mx-auto">
-                      <ThemeBtn / >
+                    <Card />  
                   </div>
             </div>
         </div>
