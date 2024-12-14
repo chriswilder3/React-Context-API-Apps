8e1c79f5ad4b36db3246b8c37d2986e91fe2dbb8
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-14 19:22:38 +0530

    created ThemeContext and its provider

--- Theme-Switcher-App/src/App.jsx
+++ Theme-Switcher-App/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 
 import './App.css'
+import { ThemeProvider } from './contexts/Theme'
 
 function App() {
   
 
   return (
     <>
-      <h1> Hello </h1>
+      <ThemeProvider>
+        <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                  <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                          
+                      </div>
+
+                      <div className="w-full max-w-sm mx-auto">
+                        
+                  </div>
+            </div>
+        </div>
+      </ThemeProvider>
+
     </>
   )
 }
